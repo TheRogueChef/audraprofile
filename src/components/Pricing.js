3657b20f326c9f5a60200cb3cc3453c0ca4f953d
Erik Carlson <theroguechef@comcast.net>
2023-08-16 15:48:14 -0700

    fixed Services Disclaimer

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -5,53 +5,57 @@ import '../components/style.css';
 
 const Pricing = () => {
     return (
-        <div className='priceShell'  >
-            <br />
-            <h1>Services</h1>
-            <br />
-            <div >
-                <ul style={{ textAlign: 'inherit', listStyle: 'none'}}>Cuts
-                <br /><br />
-                    <li>Mens = $50.00</li>
-                    <li>Womens = starting at $75.00</li>
-                    <li>Kids = $45.00</li>
-                </ul>
+        <div className='AMShell'>
+            <div className='priceShell'  >
+                <br />
+                <div>
+                    <h1>Services</h1>
+                </div>
+                <br />
+                <div >
+                    <ul style={{ textAlign: 'inherit', listStyle: 'none' }}>Cuts
+                        <br /><br />
+                        <li>Mens = $50.00</li>
+                        <li>Womens = starting at $75.00</li>
+                        <li>Kids = $45.00</li>
+                    </ul>
+                </div>
+                <div >
+                    <ul style={{ textAlign: 'inherit', listStyle: 'none' }}>Partial Color
+                        <br /><br />
+                        <li>Mens = $50.00</li>
+                        <li>Womens = starting at $75.00</li>
+                        <li>Kids = $45.00</li>
+                    </ul>
+                </div>
+                <div >
+                    <ul style={{ textAlign: 'inherit', listStyle: 'none' }}>Full Color
+                        <br /><br />
+                        <li>Mens = $50.00</li>
+                        <li>Womens = starting at $75.00</li>
+                        <li>Kids = $45.00</li>
+                    </ul>
+                </div>
+                <div >
+                    <ul style={{ textAlign: 'inherit', listStyle: 'none' }}>Perms
+                        <br /><br />
+                        <li>Mens = $50.00</li>
+                        <li>Womens = starting at $75.00</li>
+                        <li>Kids = $45.00</li>
+                    </ul>
+                </div>
+                <div >
+                    <ul style={{ textAlign: 'inherit', listStyle: 'none' }}>Other Services
+                        <br /><br />
+                        <li>Bang Trim = $25.00</li>
+                        <li>Root Touch Up = starting at $45.00</li>
+                        <li>Kids = $45.00</li>
+                    </ul>
+                </div>
             </div>
-            <div >
-                <ul style={{ textAlign: 'inherit', listStyle: 'none'}}>Partial Color
-                <br /><br />
-                    <li>Mens = $50.00</li>
-                    <li>Womens = starting at $75.00</li>
-                    <li>Kids = $45.00</li>
-                </ul>
+            <div>
+                <h5 style={{ fontStyle: 'italic'}}>* Prices subject to change *</h5>
             </div>
-            <div >
-                <ul style={{ textAlign: 'inherit', listStyle: 'none'}}>Full Color
-                <br /><br />
-                    <li>Mens = $50.00</li>
-                    <li>Womens = starting at $75.00</li>
-                    <li>Kids = $45.00</li>
-                </ul>
-            </div>
-            <div >
-                <ul style={{ textAlign: 'inherit', listStyle: 'none'}}>Perms
-                <br /><br />
-                    <li>Mens = $50.00</li>
-                    <li>Womens = starting at $75.00</li>
-                    <li>Kids = $45.00</li>
-                </ul>
-            </div>
-            <div >
-                <ul style={{ textAlign: 'inherit', listStyle: 'none'}}>Other Services
-                <br /><br />
-                    <li>Bang Trim = $25.00</li>
-                    <li>Root Touch Up = starting at $45.00</li>
-                    <li>Kids = $45.00</li>
-                </ul>
-            </div>
-            {/* <br  />
-            <Link to='top' style={{color:'red'}} smooth={true} duration={500}>Back to Top</Link>
-            <br  /><br  /> */}
         </div>
     )
 }
